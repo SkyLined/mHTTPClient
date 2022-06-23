b075166794d48568649158a88d324dcb20711fe9
SkyLined <cojmpjum_github.com@sxd.be>
2022-06-23 17:18:27 +0200

    Store temp files in OS temp folder

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -137,7 +137,8 @@ try:
   oConsole.fOutput("\u2500\u2500\u2500\u2500 Creating a cCertificateStore instance ".ljust(160, "\u2500"));
   
   if m0SSL is not None:
-    sCertificateAuthorityFolderPath = os.path.join(os.path.dirname(__file__), "tmp");
+    import tempfile;
+    sCertificateAuthorityFolderPath = os.path.join(tempfile.gettempdir(), "tmp");
   
     oCertificateAuthority = m0SSL.cCertificateAuthority(sCertificateAuthorityFolderPath, "mHTTP Test");
     oConsole.fOutput("  oCertificateAuthority = ", str(oCertificateAuthority));
